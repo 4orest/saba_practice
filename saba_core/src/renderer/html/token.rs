d3280d096bbd1ce8deb66543cdfb6deb4bc60dfe
stooone <4orestelements@gmail.com>
2025-01-05 00:52:57 +0900

    ScriptData状態の実装

--- saba_core/src/renderer/html/token.rs
+++ saba_core/src/renderer/html/token.rs
@@ -427,6 +427,18 @@ impl Iterator for HtmlTokenizer {
                         return Some(HtmlToken::Eof);
                     }
                 }
+                State::ScriptData => {
+                    if c == '<' {
+                        self.state = State::ScriptDataLessThanSign;
+                        continue;
+                    }
+
+                    if self.is_eof() {
+                        return Some(HtmlToken::Eof);
+                    }
+
+                    return Some(HtmlToken::Char(c));
+                }
                 _ => {}
             }
         }
