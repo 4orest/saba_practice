75cd10df0c8060e4fb76574c526dfb04415e8be4
stooone <4orestelements@gmail.com>
2025-04-05 22:42:09 +0900

    記号トークンを返す

--- saba_core/src/renderer/css/token.rs
+++ saba_core/src/renderer/css/token.rs
@@ -43,3 +43,39 @@ impl CssTokenizer {
         }
     }
 }
+
+impl Iterator for CssTokenizer {
+    type Item = CssToken;
+
+    /// https://www.w3.org/TR/css-syntax-3/#consume-token
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            if self.pos >= self.input.len() {
+                return None;
+            }
+
+            let c = self.input[self.pos];
+
+            let token = match c {
+                '(' => CssToken::OpenParenthesis,
+                ')' => CssToken::CloseParenthesis,
+                ',' => CssToken::Delim(','),
+                '.' => CssToken::Delim('.'),
+                ':' => CssToken::Colon,
+                ';' => CssToken::SemiColon,
+                '{' => CssToken::OpenCurly,
+                '}' => CssToken::CloseCurly,
+                ' ' | '\n' => {
+                    self.pos += 1;
+                    continue;
+                }
+                _ => {
+                    unimplemented!("char {} is not supported yet", c);
+                }
+            };
+
+            self.pos += 1;
+            return Some(token);
+        }
+    }
+}
