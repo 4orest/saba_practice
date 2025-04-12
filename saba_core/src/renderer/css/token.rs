33451a253e442a58a4a192ddae931c0c1bde088a
stooone <4orestelements@gmail.com>
2025-04-12 22:22:32 +0900

    識別子トークンを返す

--- saba_core/src/renderer/css/token.rs
+++ saba_core/src/renderer/css/token.rs
@@ -96,6 +96,26 @@ impl CssTokenizer {
 
         num
     }
+
+    /// https://www.w3.org/TR/css-syntax-3/#consume-ident-like-token
+    /// https://www.w3.org/TR/css-syntax-3/#consume-name
+    fn consume_ident_token(&mut self) -> String {
+        let mut s = String::new();
+        s.push(self.input[self.pos]);
+
+        loop {
+            self.pos += 1;
+            let c = self.input[self.pos];
+            match c {
+                'a'..='z' | 'A'..='Z' | '0'..='9' | '-' | '_' => {
+                    s.push(c);
+                }
+                _ => break,
+            }
+        }
+
+        s
+    }
 }
 
 impl Iterator for CssTokenizer {
@@ -125,6 +145,41 @@ impl Iterator for CssTokenizer {
                 }
                 '0'..='9' => {
                     let t = CssToken::Number(self.consume_numeric_token());
+                    self.pos -= 1;
+                    t
+                }
+                '#' => {
+                    // 本書では、常に #IDの形式のIDセレクタとして扱う。
+                    let value = self.consume_ident_token();
+                    self.pos -= 1;
+                    CssToken::HashToken(value)
+                }
+                '-' => {
+                    // 本書では負の数は取り扱わないため、ハイフンは識別子の
+                    // 一つとして扱う。
+                    let t = CssToken::Ident(self.consume_ident_token());
+                    self.pos -= 1;
+                    t
+                }
+                '@' => {
+                    // 次の3文字が識別子として有効な文字の場合、<at-keyword-token>
+                    // トークンを作成して返す。
+                    // それ以外の場合、<delim-token>を返す。
+                    if self.input[self.pos + 1].is_ascii_alphabetic()
+                        && self.input[self.pos + 2].is_alphanumeric()
+                        && self.input[self.pos + 3].is_alphanumeric()
+                    {
+                        // skip '@'
+                        self.pos += 1;
+                        let t = CssToken::AtKeyword(self.consume_ident_token());
+                        self.pos -= 1;
+                        t
+                    } else {
+                        CssToken::Delim('@')
+                    }
+                }
+                'a'..='z' | 'A'..='Z' | '_' => {
+                    let t = CssToken::Ident(self.consume_ident_token());
                     self.pos -= 1;
                     t
                 }
