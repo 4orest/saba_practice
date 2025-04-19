57d313e978fc547f18b637b5e48f30a5aecd3d14
stooone <4orestelements@gmail.com>
2025-04-19 22:24:12 +0900

    CSSOMノード、ルートノードの作成

--- saba_core/src/renderer/css/cssom.rs
+++ saba_core/src/renderer/css/cssom.rs
@@ -1,4 +1,5 @@
 use crate::renderer::css::token::CssTokenizer;
+use alloc::vec::Vec;
 use core::iter::Peekable;
 
 #[derive(Debug, Clone)]
@@ -9,5 +10,21 @@ pub struct CssParser {
 impl CssParser {
     pub fn new(t: CssTokenizer) -> Self {
         Self { t: t.peekable() }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct StyleSheet {
+    /// https://drafts.csswg.org/cssom/#dom-cssstylesheet-cssrules
+    pub rules: Vec<QualifiedRule>,
+}
+
+impl StyleSheet {
+    pub fn new() -> Self {
+        Self { rules: Vec::new() }
+    }
+
+    pub fn set_rules(&mut self, rules: Vec<QualifiedRule>) {
+        self.rules = rules;
     }
 }
