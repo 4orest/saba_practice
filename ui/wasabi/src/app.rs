6831951d35c5419e614fcdf408d402982ae66117
stooone <4orestelements@gmail.com>
2025-07-26 22:34:30 +0900

    背景となる白い四角を描画する

--- ui/wasabi/src/app.rs
+++ ui/wasabi/src/app.rs
@@ -1,1 +1,30 @@
+use alloc::{rc::Rc, string::ToString};
+use core::cell::RefCell;
+use noli::window::Window;
+use saba_core::{
+    browser::Browser,
+    constants::{WHITE, WINDOW_HEIGHT, WINDOW_INIT_X_POS, WINDOW_INIT_Y_POS, WINDOW_WIDTH},
+};
 
+#[derive(Debug)]
+pub struct WasabiUI {
+    browser: Rc<RefCell<Browser>>,
+    window: Window,
+}
+
+impl WasabiUI {
+    pub fn new(browser: Rc<RefCell<Browser>>) -> Self {
+        Self {
+            browser,
+            window: Window::new(
+                "saba".to_string(),
+                WHITE,
+                WINDOW_INIT_X_POS,
+                WINDOW_INIT_Y_POS,
+                WINDOW_WIDTH,
+                WINDOW_HEIGHT,
+            )
+            .unwrap(),
+        }
+    }
+}
