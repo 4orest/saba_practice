df18269407079e9ca149d67ff6ff007726d1972e
stooone <4orestelements@gmail.com>
2025-08-02 22:34:08 +0900

    マウスの位置を取得する

--- ui/wasabi/src/app.rs
+++ ui/wasabi/src/app.rs
@@ -1,6 +1,10 @@
 use alloc::{format, rc::Rc, string::ToString};
 use core::cell::RefCell;
 use noli::error::Result as OsResult;
+use noli::prelude::SystemApi;
+use noli::println;
+use noli::sys::api::MouseEvent;
+use noli::sys::wasabi::Api;
 use noli::window::StringSize;
 use noli::window::Window;
 use saba_core::browser::Browser;
@@ -92,7 +96,20 @@ impl WasabiUI {
     }
 
     fn run_app(&mut self) -> Result<(), Error> {
-        // 後ほど実装
+        loop {
+            self.handle_mouse_input()?;
+        }
+    }
+
+    fn handle_mouse_input(&mut self) -> Result<(), Error> {
+        if let Some(MouseEvent {
+            button: _button,
+            position,
+        }) = Api::get_mouse_cursor_info()
+        {
+            println!("mouse position {:?}", position);
+        }
+
         Ok(())
     }
 }
