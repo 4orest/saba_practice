89075266a53b66935996227a9c406fc930be17c0
stooone <4orestelements@gmail.com>
2025-08-31 16:52:28 +0900

    次のトークンを返す関数の実装

--- saba_core/src/renderer/js/token.rs
+++ saba_core/src/renderer/js/token.rs
@@ -13,6 +13,60 @@ impl JsLexer {
             input: js.chars().collect(),
         }
     }
+
+    fn consume_number(&mut self) -> u64 {
+        let mut num = 0;
+        loop {
+            if self.pos >= self.input.len() {
+                return num;
+            }
+
+            let c = self.input[self.pos];
+
+            match c {
+                '0'..='9' => {
+                    num = num * 10 + (c.to_digit(10).unwrap() as u64);
+                    self.pos += 1;
+                }
+                _ => break,
+            }
+        }
+
+        num
+    }
+}
+
+impl Iterator for JsLexer {
+    type Item = Token;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.pos >= self.input.len() {
+            return None;
+        }
+
+        // ホワイトスペースまたは改行文字が続く限り、次の位置に進める
+        while self.input[self.pos] == ' ' || self.input[self.pos] == '\n' {
+            self.pos += 1;
+
+            if self.pos >= self.input.len() {
+                return None;
+            }
+        }
+
+        let c = self.input[self.pos];
+
+        let token = match c {
+            '+' | '-' | ';' | '=' | '(' | ')' | '{' | '}' | ',' | '.' => {
+                let t = Token::Punctoator(c);
+                self.pos += 1;
+                t
+            }
+            '0'..='9' => Token::Number(self.consume_number()),
+            _ => unimplemented!("char {:?} is not supported yet", c),
+        };
+
+        Some(token)
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
