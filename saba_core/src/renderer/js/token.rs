1d6adc5d0aac447986fcab8595c883ae1bd45361
stooone <4orestelements@gmail.com>
2025-08-31 16:35:52 +0900

    JsLexer 構造体の作成

--- saba_core/src/renderer/js/token.rs
+++ saba_core/src/renderer/js/token.rs
@@ -1,3 +1,20 @@
+use alloc::string::String;
+use alloc::vec::Vec;
+
+pub struct JsLexer {
+    pos: usize,
+    input: Vec<char>,
+}
+
+impl JsLexer {
+    pub fn new(js: String) -> Self {
+        Self {
+            pos: 0,
+            input: js.chars().collect(),
+        }
+    }
+}
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Token {
     /// https://262.ecma-international.org/#sec-punctuators
